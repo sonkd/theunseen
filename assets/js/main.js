579058584f80192abd00833b4e3ee4bda8d66349
Kim Dinh Son <kimdinhson.hust.k55@gmail.com>
2024-05-29 20:43:35 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,39 +25,60 @@
 
     cbox.addEventListener("change", function () {
       const area = document.querySelector(".wrapper");
+      const menu_icon = document.querySelector(".menu-icon");
+      const close_icon = document.querySelector(".close-icon");
+
       this.checked
-        ? area.classList.add("blurry")
-        : area.classList.remove("blurry");
+        ? (area.classList.add("blurry"), close_icon.removeAttribute("hidden"),
+        menu_icon.setAttribute("hidden", ""))
+        : (area.classList.remove("blurry"), menu_icon.removeAttribute("hidden"),
+        close_icon.setAttribute("hidden", ""));
     });
   };
 
   // Check isReading state
-  const reading = document.getElementById("post");
-  var currentPos = 0;
+  if (document.getElementById("post")) {
+    const reading = document.getElementById("post");
+    var currentPos = 0;
 
-  reading.addEventListener("scroll", function() {
-    if (reading.scrollTop > currentPos) {
-      document.querySelector("#navpost").setAttribute("hidden", "");
-      currentPos = reading.scrollTop;
-    } else {
-      document.querySelector("#navpost").removeAttribute("hidden");
-      currentPos = reading.scrollTop;
-    }
-  });
+    reading.addEventListener("scroll", function () {
+      if (reading.scrollTop > currentPos) {
+        document.querySelector("#navpost").setAttribute("hidden", "");
+        currentPos = reading.scrollTop;
+      } else {
+        document.querySelector("#navpost").removeAttribute("hidden");
+        currentPos = reading.scrollTop;
+      }
+    });
+  };
 
   //Share popup
-  const viewBtn = document.querySelector("#view-modal"),
+  const viewBtn = document.querySelector(".view-modal"),
     popup = document.querySelector(".popup-share"),
     close = popup.querySelector(".share-close"),
     field = popup.querySelector(".share-field"),
     input = field.querySelector("input"),
     copy = field.querySelector("button");
-
+  
+  const viewShareIcon = document.querySelector(".view-modal"),
+    popup2 = document.querySelector(".popup-share"),
+    close2 = popup2.querySelector(".share-close"),
+    field2 = popup2.querySelector(".share-field"),
+    input2 = field2.querySelector("input"),
+    copy2 = field2.querySelector("#share-icon");
+  
   viewBtn.onclick = () => {
     popup.classList.toggle("show");
   }
   close.onclick = () => {
     viewBtn.click();
+  }
+
+  viewShareIcon.onclick = () => {
+    popup.classList.toggle("show");
+  }
+  close2.onclick = () => {
+    viewShareIcon.click();
   }
 
   copy.onclick = () => {
@@ -69,6 +90,19 @@
         window.getSelection().removeAllRanges(); //remove selection from page
         field.classList.remove("active");
         copy.innerText = "Copy";
+      }, 3000);
+    }
+  }
+
+  copy2.onclick = () => {
+    input2.select(); //select input value
+    if (document.execCommand("copy")) { //if the selected text is copied
+      field2.classList.add("active");
+      copy2.innerText = "Copied";
+      setTimeout(() => {
+        window.getSelection().removeAllRanges(); //remove selection from page
+        field2.classList.remove("active");
+        copy2.innerText = "Copy";
       }, 3000);
     }
   }
